8c3dcfb59a21432f014a2be7bdcc661a61fe00f7
margo <aidanmclg@gmail.com>
2025-07-11 13:57:13 -0400

    eya

---
+++ part1/src/App.jsx
@@ -0,0 +1,62 @@
+const Header = (props) => {
+  return (
+    <>
+      <p>{props.course}</p>
+    </>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <>
+      <p>
+        {props.partName}: {props.partNum}
+      </p>
+    </>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <>
+      <Part partName={props.part1} partNum={props.exercises1} />
+      <Part partName={props.part2} partNum={props.exercises2} />
+      <Part partName={props.part3} partNum={props.exercises3} />
+    </>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <>
+      <p>Number of exercises: {props.total}</p>
+    </>
+  );
+};
+
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
+      <Total total={exercises1 + exercises2 + exercises3} />
+    </div>
+  );
+};
+
+export default App;
